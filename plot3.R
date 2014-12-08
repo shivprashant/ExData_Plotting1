40698bacbd984a772cf887338d5a307fabe8581d
Shiv Prashant Sood <shivprashant@outlook.com>
2014-12-07 16:07:30 -0800

    bug fixes

--- plot3.R
+++ plot3.R
@@ -14,17 +14,23 @@ df<-read.delim("household_power_consumption.txt", header=TRUE, sep=";", na.strin
 df_subset<-df[(as.Date(df$Date,format="%d/%m/%Y") == as.Date('1/2/2007',format="%d/%m/%Y")) | 
               (as.Date(df$Date,format="%d/%m/%Y") == as.Date('2/2/2007',format="%d/%m/%Y")),]
 
-# Draw a line plot
-#hist(df_subset$Global_active_power,col='red',xlab='Global Active Power (kilowatts)',
-#     main="Global Active Power")
-
+# X-Axis is Time for 2 choosen days. Format to DataAndtime Object that can 
+# be used by the plot functions.
 time<-paste(df_subset$Date,df_subset$Time)
 df_subset$Time<-strptime(time,"%d/%m/%Y %H:%M:%S")
+
+#Open a png device type.
 png(file="plot3.png", height=480, width=480)
+
+#Plot the relevant graphs.
+#1- Create a plot with sub_metering_1.
+#2- Add lines additional lines for sub_metering_1, sub_metering_3
+#3 - Add legend
 plot(df_subset$Time,df_subset$Sub_metering_1,"l",xlab="",ylab="Energy Sub metering")
 lines(df_subset$Time,df_subset$Sub_metering_2,xlab="",col="red")
 lines(df_subset$Time,df_subset$Sub_metering_3,xlab="", col="blue")
-legend("topright",c=("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), lwd=c(2.5,2.5), 
-       col= c("black", "red", "blue"));
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), lwd=c(2.5,2.5),col=c("black", "red", "blue"))
+
+# Close devices and check out the cool graphs!!
 dev.off()
 
